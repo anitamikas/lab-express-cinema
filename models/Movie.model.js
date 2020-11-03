01f51521f48eaaed21d70b3cfbf3aed2fcd5350d
Anita Mikas <mikasanita@gmail.com>
2020-11-03 22:00:05 +0100

    done

---
+++ models/Movie.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+ 
+const movieSchema = new Schema(
+  {
+    title: String,
+    director: String,
+    stars:[String],
+    image: String,
+    description: String,
+    showtimes: [String],
+  },
+  {
+    timestamps: true
+  }
+);
+ 
+// module.exports = model('Movie', movieSchema);
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
 module.exports = router;
+
+
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+        .then(movies => {
+            console.log('Retrieved books from DB:', movies);
+            res.render('movies', {movies : movies});
+        })
+        .catch(error => console.log('Error while getting the books from the DB: ', error));
+});
+
+router.get('/movies/:movieId', (req, res, next) => {
+    Movie.findById(req.params.movieId)
+        .then(movies => { 
+            console.log(movies);
+                res.render('movieDetail', { movieFromDB: movies});
+              }, function(err) {
+                console.log('Something went wrong!', err);
+              })
+              .catch(err => console.log('The error while searching artists occurred: ', err));
+            });
+
+
